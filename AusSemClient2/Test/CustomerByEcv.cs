846390feefda2312e024a01d50d248882bb8689a
ABS\fikesk <fikesk@infoconsulting.eu>
2024-11-30 13:19:31 +0100

    Implemented custbyid and custbxecv

--- AusSemClient2/Test/CustomerByEcv.cs
+++ AusSemClient2/Test/CustomerByEcv.cs
@@ -1,13 +1,28 @@
 using System.Text;
 
-class CustomerByEcv :CustomerByKey
+class CustomerByEcv : IRecord<CustomerByEcv>, IExtendRec
 {
-    public CustomerByEcv(int id, string ecv, long address) : base(id, ecv, address)
-    {
+    private int _id;
+    private string _ecv = "xxxxxxxxxx";
+    private static readonly int _maxEcvSize = 10;
+    long _address;
 
+    public int Id { get => _id; set => _id = value; }
+    public string Ecv { get => _ecv; set => _ecv = value; }
+    public long Address { get => _address; set => _address = value; }
+    public static int MaxEcvSize => _maxEcvSize;
+
+    public CustomerByEcv() {
+        
     }
 
-    public override byte[] GetByteKey()
+    public CustomerByEcv(int id, string ecv, long address) {
+        _id = id;
+        _ecv = ecv;
+        _address = address;
+    }
+
+    public byte[] GetByteKey()
     {
         using (var memoryStream = new MemoryStream())
         { 
@@ -18,13 +33,79 @@ class CustomerByEcv :CustomerByKey
         }
     }
 
-    public override bool KeyUpdated(CustomerByKey other)
+    public bool KeyUpdated(CustomerByEcv other)
     {
         return Ecv != other.Ecv;
     }
 
-    public override bool Equals(CustomerByKey other)
+    public bool Equals(CustomerByEcv other)
     {
         return Ecv == other.Ecv;
+    }
+
+    public bool Equals()
+    {
+        return false;
+    }
+
+    public int GetSize()
+    {
+        int size = 0;
+
+        size += sizeof(int);
+        size += sizeof(long);
+
+        if (_ecv != null)
+        {
+            size +=  _maxEcvSize * sizeof(char);
+        }
+
+        return size;
+    }
+
+    public CustomerByEcv CreateInstance()
+    {
+        return new CustomerByEcv(-1, "xxxxxxxxxx", -1);
+    }
+
+    public byte[] GetByteArray()
+    {
+         using (var memoryStream = new MemoryStream())
+        { 
+            memoryStream.Write(BitConverter.GetBytes(_id), 0, sizeof(int));
+
+            string paddedEcv = _ecv?.PadRight(_maxEcvSize, '\0') ?? new string('\0', _maxEcvSize);
+            byte[] ecvBytes = Encoding.Unicode.GetBytes(paddedEcv);
+            memoryStream.Write(ecvBytes, 0, ecvBytes.Length);
+
+            memoryStream.Write(BitConverter.GetBytes(_address), 0, sizeof(long));
+
+            return memoryStream.ToArray();
+        }
+    }
+
+    public void FromByteArray(byte[] bytes)
+    {
+        using (var memoryStream = new MemoryStream(bytes))
+        {
+            byte[] intBuffer = new byte[sizeof(int)];
+            memoryStream.Read(intBuffer, 0, sizeof(int));
+            _id = BitConverter.ToInt32(intBuffer, 0);
+
+            byte[] ecvBytes = new byte[_maxEcvSize * sizeof(char)];
+            memoryStream.Read(ecvBytes, 0, ecvBytes.Length);
+            _ecv = Encoding.Unicode.GetString(ecvBytes).TrimEnd('\0');
+
+           byte[] longBuffer = new byte[sizeof(long)];
+            memoryStream.Read(longBuffer, 0, sizeof(long));
+            _address = BitConverter.ToInt32(longBuffer, 0);
+        }
+    }
+
+    public void Update(CustomerByEcv other)
+    {
+        _id = _id != other.Id ? other.Id : _id;
+        _ecv = _ecv != other.Ecv ? other.Ecv : _ecv;
+        _address = _address != other.Address ? other.Address : _address;
     }
 }
--- AusSemClient2/Test/CustomerById.cs
+++ AusSemClient2/Test/CustomerById.cs
@@ -1,11 +1,77 @@
-class CustomerById : CustomerByKey
-{
-    public CustomerById(int id, string ecv, long address) : base(id, ecv, address)
-    {
+using System.Text;
 
+class CustomerById : IRecord<CustomerById>, IExtendRec
+{
+    private int _id;
+    private string _ecv = "xxxxxxxxxx";
+    private static readonly int _maxEcvSize = 10;
+    long _address;
+
+    public int Id { get => _id; set => _id = value; }
+    public string Ecv { get => _ecv; set => _ecv = value; }
+    public long Address { get => _address; set => _address = value; }
+    public static int MaxEcvSize => _maxEcvSize;
+
+    public CustomerById() {
+        
     }
 
-    public override byte[] GetByteKey()
+    public CustomerById(int id, string ecv, long address) {
+        _id = id;
+        _ecv = ecv;
+        _address = address;
+    }
+
+    public int GetSize()
+    {
+        int size = 0;
+
+        size += sizeof(int);
+        size += sizeof(long);
+
+        if (_ecv != null)
+        {
+            size +=  _maxEcvSize * sizeof(char);
+        }
+
+        return size;
+    }
+
+    public byte[] GetByteArray()
+    {
+         using (var memoryStream = new MemoryStream())
+        { 
+            memoryStream.Write(BitConverter.GetBytes(_id), 0, sizeof(int));
+
+            string paddedEcv = _ecv?.PadRight(_maxEcvSize, '\0') ?? new string('\0', _maxEcvSize);
+            byte[] ecvBytes = Encoding.Unicode.GetBytes(paddedEcv);
+            memoryStream.Write(ecvBytes, 0, ecvBytes.Length);
+
+            memoryStream.Write(BitConverter.GetBytes(_address), 0, sizeof(long));
+
+            return memoryStream.ToArray();
+        }
+    }
+
+    public void FromByteArray(byte[] bytes)
+    {
+        using (var memoryStream = new MemoryStream(bytes))
+        {
+            byte[] intBuffer = new byte[sizeof(int)];
+            memoryStream.Read(intBuffer, 0, sizeof(int));
+            _id = BitConverter.ToInt32(intBuffer, 0);
+
+            byte[] ecvBytes = new byte[_maxEcvSize * sizeof(char)];
+            memoryStream.Read(ecvBytes, 0, ecvBytes.Length);
+            _ecv = Encoding.Unicode.GetString(ecvBytes).TrimEnd('\0');
+
+           byte[] longBuffer = new byte[sizeof(long)];
+            memoryStream.Read(longBuffer, 0, sizeof(long));
+            _address = BitConverter.ToInt32(longBuffer, 0);
+        }
+    }
+
+    public byte[] GetByteKey()
     {
         using (var memoryStream = new MemoryStream())
         { 
@@ -14,13 +80,30 @@ class CustomerById : CustomerByKey
         }
     }
 
-    public override bool KeyUpdated(CustomerByKey other)
+    public bool KeyUpdated(CustomerById other)
     {
         return Id != other.Id;
     }
 
-    public override bool Equals(CustomerByKey other)
+    public bool Equals(CustomerById other)
     {
         return Id == other.Id;
+    }
+
+    public CustomerById CreateInstance()
+    {
+        return new CustomerById(-1, "xxxxxxxxxx", -1);
+    }
+
+    public void Update(CustomerById other)
+    {
+        _id = _id != other.Id ? other.Id : _id;
+        _ecv = _ecv != other.Ecv ? other.Ecv : _ecv;
+        _address = _address != other.Address ? other.Address : _address;
+    }
+
+    public bool Equals()
+    {
+        return false;
     }
 }
